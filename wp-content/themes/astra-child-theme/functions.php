8a63750e4f17d645fa8b753ad149739d75664c85
Murat Kilic <muratkilic1978@gmail.com>
2020-10-16 23:09:31 +0200

    Child Theme was updated

--- wp-content/themes/astra-child-theme/functions.php
+++ wp-content/themes/astra-child-theme/functions.php
@@ -2,6 +2,8 @@
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
+    
+    wp_enqueue_style( 'AnimateCSSCDN', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
     $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
     $theme = wp_get_theme();
     wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
